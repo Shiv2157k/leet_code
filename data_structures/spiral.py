dbe5521eb1ac038a28ac3c83d12d3dea025ffff3
Shiv2157k <shiv2157.k@gmail.com>
2020-06-27 15:29:29 -0500

    feat generate n * n spiral matrix

--- data_structures/spiral.py
+++ data_structures/spiral.py
@@ -31,6 +31,24 @@ class Spiral:
                 row, col = row + row_directions[di], col + col_directions[di]
         return order
 
+    def build_spiral_matrix(self, n: int) -> List[List[int]]:
+
+        row_direction, col_direction = [0, 1, 0, -1], [1, 0, -1, 0]
+        seen = [[False] * n for _ in range(n)]
+        spiral_matrix = [[None] * n for _ in range(n)]
+        row = col = di = 0
+
+        for num in range(1, (n * n) + 1):
+            spiral_matrix[row][col] = num
+            seen[row][col] = True
+            cr, cc = row + row_direction[di], col + col_direction[di]
+            if 0 <= cr < n and 0 <= cc < n and not seen[cr][cc]:
+                row, col = cr, cc
+            else:
+                di = (di + 1) % 4
+                row, col = row + row_direction[di], col + col_direction[di]
+        return spiral_matrix
+
 
 if __name__ == "__main__":
     spiral = Spiral()
@@ -41,3 +59,4 @@ if __name__ == "__main__":
             [7, 8, 9]
         ]
     ))
+    print(spiral.build_spiral_matrix(3))
