4eeb3a8b4525a3779922a15ab1a9eee404c4a09e
Shiv2157k <shiv2157.k@gmail.com>
2020-06-28 20:30:28 -0500

    feat unique paths in a obstacle grid

--- data_structures/recurrsion_dp/unique_paths.py
+++ data_structures/recurrsion_dp/unique_paths.py
@@ -1,3 +1,4 @@
+from typing import List
 
 
 class Grid:
@@ -43,9 +44,43 @@ class Grid:
         from math import factorial
         return factorial(m + n - 2) // factorial(m - 1) // factorial(n - 1)
 
+    def get_unique_paths_with_obstacles(self, obstacle_grid: List[List[int]]) -> int:
+        """
+        Approach: Dynamic Programming.
+        Time Complexity: O(m * n)
+        Space Complexity: O(1)
+        :param obstacle_grid:
+        :return:
+        """
+
+        # base case
+        if obstacle_grid[0][0] == 1:
+            return 0
+
+        m, n, obstacle_grid[0][0] = len(obstacle_grid), len(obstacle_grid[0]), 1
+        for i in range(1, m):
+            obstacle_grid[i][0] = int(obstacle_grid[i][0] == 0 and obstacle_grid[i - 1][0] == 1)
+        for j in range(1, n):
+            obstacle_grid[0][j] = int(obstacle_grid[0][j] == 0 and obstacle_grid[0][j - 1] == 1)
+
+        for col in range(1, m):
+            for row in range(1, n):
+                if obstacle_grid[col][row] == 0:
+                    obstacle_grid[col][row] = obstacle_grid[col - 1][row] + obstacle_grid[col][row - 1]
+                else:
+                    obstacle_grid[col][row] = 0
+        return obstacle_grid[m - 1][n - 1]
+
 
 if __name__ == "__main__":
     grid = Grid()
     print(grid.get_unique_paths(7, 3))
     print(grid.get_unique_paths_(7, 3))
     print(grid.get_unique_paths__(7, 3))
+    print(grid.get_unique_paths_with_obstacles(
+        [
+            [0, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0]
+        ]
+    ))
