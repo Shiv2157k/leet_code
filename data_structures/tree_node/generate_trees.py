ebaab73c536aa11a3de69ea25a7c6bd3ec39899f
Shiv2157k <shiv2157.k@gmail.com>
2020-07-06 20:08:47 -0500

    feat check if binary search tree is valid

--- data_structures/tree_node/generate_trees.py
+++ data_structures/tree_node/generate_trees.py
@@ -11,6 +11,13 @@ class TreeNode:
 class Trees:
 
     def generate_trees(self, n: int) -> List[TreeNode]:
+        """
+        Approach: Recursion
+        Time Complexity: O(4^n / n ^ 1/2)
+        Space Complexity: O(4^n / n ^ 1/2)
+        :param n:
+        :return:
+        """
 
         def generate(left: int, right: int) -> List[TreeNode]:
             # Base Case
@@ -33,7 +40,38 @@ class Trees:
 
         return generate(1, n) if n else []
 
+    def number_of_unique_bst(self, n: int) -> int:
+        """
+        Approach: DP
+        Time Complexity: O(n^2)
+        Space Complexity: O(n)
+        :param n:
+        :return:
+        """
+
+        G = [0] * (n + 1)
+        G[0], G[1] = 1, 1
+        for i in range(2, n + 1):
+            for j in range(1, i + 1):
+                G[i] += G[j - 1] * G[i - j]
+        return G[n]
+
+    def number_of_unique_bst(self, n: int) -> int:
+        """
+        Approach: Mathematical Deduction
+        Formulae: G(n + 1) = 2 * (2n + 1) / (n + 2)
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        :param n:
+        :return:
+        """
+        C = 1
+        for i in range(0, n):
+            C = C * 2 * (2 * i + 1) / (i + 2)
+        return int(C)
+
 
 if __name__ == "__main__":
     tree = Trees()
     print(tree.generate_trees(3))
+    print(tree.number_of_unique_bst(3))
---
+++ data_structures/tree_node/valid_bst.py
@@ -0,0 +1,78 @@
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class BinarySearchTree:
+
+    def is_valid(self, root: TreeNode) -> bool:
+        """
+        Approach: DFS (Recursion)
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        :param root:
+        :return:
+        """
+
+        def helper(node, lower=float("-inf"), upper=float("inf")) -> bool:
+            if not node:
+                return True
+            val = node.val
+            if val <= lower or val >= upper:
+                return False
+            if not helper(node.left, lower, val):
+                return False
+            if not helper(node.right, val, upper):
+                return False
+        return helper(root)
+
+    def is_valid_(self, root: TreeNode) -> bool:
+        """
+        Approach: BFS (Iteration using stack)
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        :param root:
+        :return:
+        """
+
+        if not root:
+            return True
+
+        stack = [(root, float("-inf"), float("inf")), ]
+
+        while stack:
+            root, lower, upper = stack.pop()
+
+            if not root:
+                continue
+            val = root.val
+            if val <= lower or val >= upper:
+                return False
+            stack.append((root.left, lower, val))
+            stack.append((root.right, val, upper))
+        return True
+
+    def is_valid__(self, root: TreeNode) -> bool:
+        """
+        Approach: Iteration with In-order traversal.
+        i.e., Left -> Node -> Right
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        :param root:
+        :return:
+        """
+        stack, in_order = [], float("-inf")
+
+        while stack or not root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if root.val <= in_order:
+                return False
+            in_order = root.val
+            root = root.right
+        return True
+
