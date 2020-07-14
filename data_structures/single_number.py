db1d24ca9869858cb7843e005e6828c68f90dd43
Shiv2157k <shiv2157.k@gmail.com>
2020-07-14 18:55:25 -0500

    feat get single number in a 3 duplicated list

--- data_structures/single_number.py
+++ data_structures/single_number.py
@@ -35,8 +35,52 @@ class Single:
             res ^= num
         return res
 
+    def number_ii(self, nums: List[int]) -> int:
+        """
+        Approach: Using hashset
+        Time Complexity: O(N)
+        Space Complexity: O(N)
+        :param nums:
+        :return:
+        """
+        return (3 * sum(set(nums)) - sum(nums)) // 2
+
+    def number__ii(self, nums: List[int]) -> int:
+        """
+        Approach: Using Hash Map
+        Time Complexity: O(N)
+        Space Complexity: O(N)
+        :param nums:
+        :return:
+        """
+        from collections import Counter
+        hashmap = Counter(nums)
+        for num in hashmap.keys():
+            if hashmap[num] == 1:
+                return num
+
+    def number___ii(self, nums: List[int]) -> int:
+        """
+        Approach: Bit-wise Operator
+        Time Complexity: O(N)
+        Space Complexity: O(1)
+        Formulae:
+        l' = ~h & (l ^ i)
+        h' = ~l' & (h ^ i)
+        :param nums:
+        :return:
+        """
+        once = twice = 0
+        for num in nums:
+            once = ~twice & (once ^ num)
+            twice = ~once & (twice ^ num)
+        return once
+
 
 if __name__ == "__main__":
     single = Single()
     print(single.get_number_([4, 5, 6, 4, 5, 6, 9, 10, 10, 11, 11]))
     print(single.get_number([4, 5, 6, 4, 5, 6, 9, 10, 10, 11, 11]))
+    print(single.number___ii([1, 1, 1, 5, 5, 5, 99]))
+    print(single.number__ii([1, 1, 1, 5, 5, 5, 99]))
+    print(single.number_ii([1, 1, 1, 5, 5, 5, 99]))
