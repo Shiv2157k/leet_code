6fd93f4f5adeb0e467665c2bce53caaef57e3100
Shiv2157k <shiv2157.k@gmail.com>
2020-07-16 18:26:28 -0500

    feat linked list cycle II

--- data_structures/linked_list/has_cycle.py
+++ data_structures/linked_list/has_cycle.py
@@ -26,3 +26,53 @@ class LinkedList:
             slow = slow.next
             fast = fast.next.next
         return True
+
+    def detect_cycle(self, head: ListNode) -> ListNode:
+        """
+        Approach: Using HashSet
+        Time Complexity: O(N)
+        Space Complexity: O(N)
+        :param head:
+        :return:
+        """
+        if not head:
+            return None
+        visited, node = set(), head
+        while node:
+            if node in visited:
+                return node
+            else:
+                visited.add(node)
+                node = node.next
+        return None
+
+    def get_intersection(self, head: ListNode) -> ListNode:
+        hare = tortoise = head
+        while hare and hare.next:
+            if tortoise == hare:
+                return tortoise
+            tortoise = tortoise.next
+            hare = hare.next.next
+        return None
+
+    def detect_cycle_(self, head: ListNode) -> ListNode:
+        """
+        Approach: Floyd's tortoise and hare.
+        Time Complexity: O(N)
+        Space Complexity: O(1)
+        :param head:
+        :return:
+        """
+        if not head:
+            return None
+
+        meeting_point = self.get_intersection(head)
+
+        if not meeting_point:
+            return None
+
+        pointer1, pointer2 = head, meeting_point
+        while pointer1 != pointer2:
+            pointer1 = pointer1.next
+            pointer2 = pointer2.next
+        return pointer1
