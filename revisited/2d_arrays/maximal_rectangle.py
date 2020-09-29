4c73e68e8f5a74d7996cd4bfe60f3c6b1157fcf6
Shiv2157k <shiv2157.k@gmail.com>
2020-09-28 22:16:04 -0500

    feat maximal rectangle in a matrix at maxial area at each point

--- revisited/2d_arrays/maximal_rectangle.py
+++ revisited/2d_arrays/maximal_rectangle.py
@@ -3,6 +3,51 @@ from typing import List
 
 class Rectangle:
 
+    def get_maximum_area(self, matrix: List[List[str]]) -> int:
+        """
+        Approach: Maximum Area at each point.
+        Time Complexity: O(MN)
+        Space Complexity: O(N)
+        :param matrix:
+        :return:
+        """
+        if not matrix:
+            return 0
+
+        m, n = len(matrix), len(matrix[0])
+        left = [0] * n
+        right = [n] * n
+        height = [0] * n
+        max_area = 0
+
+        for i in range(m):
+            curr_left, curr_right = 0, n
+
+            # update height
+            for j in range(n):
+                height[j] = height[j] + 1 if matrix[i][j] == "1" else 0
+
+            # update left
+            for j in range(n):
+                if matrix[i][j] == "1":
+                    left[j] = max(left[j], curr_left)
+                else:
+                    left[j] = 0
+                    curr_left = j + 1
+
+            # update right
+            for j in range(n - 1, -1, -1):
+                if matrix[i][j] == "1":
+                    right[j] = min(right[j], curr_right)
+                else:
+                    right[j] = n
+                    curr_right = j
+
+            # calculate max area
+            for j in range(n):
+                max_area = max(max_area, height[j] * (right[j] - left[j]))
+        return max_area
+
     def histogram(self, heights: List[int]):
         """
         Approach: Using Stack
@@ -42,6 +87,14 @@ class Rectangle:
 
 if __name__ == "__main__":
     rectangle = Rectangle()
+    print(rectangle.get_maximum_area(
+        [
+            ["1", "0", "1", "0", "0"],
+            ["1", "0", "1", "1", "1"],
+            ["1", "1", "1", "1", "1"],
+            ["1", "0", "0", "1", "0"]
+        ]
+    ))
     print(rectangle.get_maximal_with_1(
         [
             ["1", "0", "1", "0", "0"],
