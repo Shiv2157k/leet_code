1115ca453df1713eeda5dab296543f82d439fb67
Shiv2157k <shiv2157.k@gmail.com>
2020-11-03 16:10:27 -0600

    feat re-visit decode ways

--- revisited/math_and_strings/recursion_memoization_dp/decode_ways.py
+++ revisited/math_and_strings/recursion_memoization_dp/decode_ways.py
@@ -13,18 +13,24 @@ class Decode:
         :param string:
         :return:
         """
-
+        # base case
         if not string:
             return 0
-
         dp = [0 for _ in range(len(string) + 1)]
+        # initiate the first index to 1
         dp[0] = 1
+        # initiate the second index based on the
+        # first digit
         dp[1] = 0 if string[0] == "0" else 1
 
+        # loop from the third index of dp
         for i in range(2, len(dp)):
+            # check the single digit and add respectively
             if string[i - 1] != "0":
                 dp[i] += dp[i - 1]
+            # check for double digit numbers
             two_digit = int(string[i - 2: i])
+            # check if it is in valid range
             if 10 <= two_digit <= 26:
                 dp[i] += dp[i - 2]
         return dp[len(string)]
@@ -33,7 +39,7 @@ class Decode:
 
         # if index is equals to length of string
         # break the recursion by returning 1
-        if len(string) == index:
+        if index == len(string):
             return 1
 
         # if given string index has 0 it is not valid fr decoding
@@ -46,16 +52,19 @@ class Decode:
 
         if index in self.memo:
             return self.memo[index]
+
         # recursion call
-        ans = self.memoization(index + 1, string) + (self.memoization(index + 2, string) if (int(string[index: index + 2]) <= 26) else 0)
+        ans = self.memoization(index + 1, string) \
+            + (self.memoization(index + 2, string) if (int(string[index: index + 2]) <= 26) else 0)
+
         # save it for memoization
         self.memo[index] = ans
         return ans
 
     def get_number_of_ways(self, string: str) -> int:
+        # validation
         if not string:
             return 0
-
         return self.memoization(0, string)
 
 
