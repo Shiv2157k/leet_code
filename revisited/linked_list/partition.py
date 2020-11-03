1fd060f981cf60d049b8f3859ef2690cf559bf2a
Shiv2157k <shiv2157.k@gmail.com>
2020-11-03 13:57:42 -0600

    feat re-visit partition of ll

--- revisited/linked_list/partition.py
+++ revisited/linked_list/partition.py
@@ -16,18 +16,30 @@ class Partition:
         :return:
         """
 
+        # dummy nodes reverse engineering and break downing
+        # input ll to left and right
         left = left_head = ListNode(0)
         right = right_head = ListNode(0)
 
         while head:
-            if head.val < x:
+            # if the node value is less than x
+            # assign it to the left dummy
+            if x < head.val:
                 left.next = head
+                # keep moving to next node of left
                 left = left.next
+            # if it is greater than or equal to x
+            # assign it to the right
             else:
                 right.next = head
+                # keep moving to next node of right
                 right = right.next
+            # traverse through the nodes in the given ll
             head = head.next
 
+        # close the right node
         right.next = None
+        # join both left and right
         left.next = right_head.next
         return left_head.next
+
