c6f7fb4f502baf7b84ab83f192b31d0ffda9918d
Shiv2157k <shiv2157.k@gmail.com>
2021-02-20 14:07:49 -0600

    feat find pivot index

---
+++ revisited_2021/arrays/find_pivot_index.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Array:
+
+    def find_pivot_index(self, nums: List[int]) -> int:
+        """
+        Approach: Prefix Sum
+        Time Complexity: O(N)
+        Space Complexity: O(1)
+        :param nums:
+        :return:
+        """
+        left_sum, total_sum = 0, sum(nums)
+        for pivot, num in enumerate(nums):
+            if left_sum == (total_sum - left_sum - num):
+                return pivot
+            left_sum += num
+        return -1
+
+
+if __name__ == "__main__":
+    array = Array()
+    print(array.find_pivot_index([2, -1, 1]))
+    print(array.find_pivot_index([1, 7, 3, 6, 5, 6]))
