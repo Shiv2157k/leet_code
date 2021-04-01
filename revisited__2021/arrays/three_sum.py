3d95c65c06f413d4805b2c2062086874ef197ef7
Shiv2157k <shiv2157.k@gmail.com>
2021-04-01 08:43:45 -0500

    feat three sum

---
+++ revisited__2021/arrays/three_sum.py
@@ -0,0 +1,55 @@
+from typing import List
+
+
+class Array:
+
+    def three_sum(self, nums: List[int]) -> List[List[int]]:
+        """
+        Approach: Two Pointers
+        Time Complexity: O(N^2)
+        Space Complexity: O(N)
+        :param nums:
+        :return:
+        """
+        if len(nums) < 3:
+            return []
+
+        result = []
+        nums.sort()
+
+        length = len(nums)
+
+        for i in range(length - 2):
+            left = i + 1
+            right = length - 1
+
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            while left < right:
+                target = nums[i] + nums[left] + nums[right]
+                if target == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif target < 0:
+                    left += 1
+                else:
+                    right -= 1
+        return result
+
+
+if __name__ == "__main__":
+    array = Array()
+    print(array.three_sum(
+        [-1, 0, 1, 2, -1, -4]
+    ))
+    print(array.three_sum(
+        []
+    ))
+    print(array.three_sum(
+        [0]
+    ))
