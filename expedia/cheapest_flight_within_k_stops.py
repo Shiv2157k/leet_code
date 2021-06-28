e8642fd5dd56f4f46127a8c62da0811a219f2b60
Shiv2157k <shiv2157.k@gmail.com>
2021-06-28 16:55:21 -0500

    feat cheapest flights with in k stops approach: bellman - ford

--- expedia/cheapest_flight_within_k_stops.py
+++ expedia/cheapest_flight_within_k_stops.py
@@ -53,9 +53,44 @@ class Flights:
 
         return -1 if result == float("inf") else result
 
+    def cheapest_flight_within_k_stops(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        """
+        Approach: Bellman Ford Algorithm
+        Time Complexity: O(K * V)
+        Space Complexity: O(V)
+        :param n:
+        :param flights:
+        :param src:
+        :param dst:
+        :param k:
+        :return:
+        """
+
+        distances = [[float("inf")] * n for _ in range(2)]
+        distances[0][src] = distances[1][src] = 0
+
+        # for iteration 0:
+        # arr 1: main array
+        # arr 2: prev array
+        # for iteration 1
+        # arr 2: main array
+        # arr 1: prev array
+        for iterations in range(k + 1):
+
+            for source, destination, cost in flights:
+
+                dU = distances[1 - iterations & 1][source]
+
+                dV = distances[iterations & 1][destination]
+
+                if dU + cost < dV:
+                    distances[iterations & 1][destination] = dU + cost
+        return -1 if distances[k & 1][dst] == float("inf") else distances[k & 1][dst]
+
 
 if __name__ == "__main__":
     f = Flights()
+
     print(f.get_cheapest_flight_within_k_stops(
         5,
         [[4, 1, 1], [1, 2, 3], [0, 3, 2], [0, 4, 10], [3, 1, 1], [1, 4, 3]],
@@ -68,4 +103,18 @@ if __name__ == "__main__":
     ))
     print(f.get_cheapest_flight_within_k_stops(
         n=3, flights=[[0, 1, 100], [1, 2, 100], [0, 2, 500]], src=0, dest=2, k=0
+    ))
+
+    print(f.cheapest_flight_within_k_stops(
+        n=3, flights=[[0, 1, 100], [1, 2, 100], [0, 2, 500]], src=0, dst=2, k=1
+    ))
+    print(f.cheapest_flight_within_k_stops(
+        n=3, flights=[[0, 1, 100], [1, 2, 100], [0, 2, 500]], src=0, dst=2, k=0
+    ))
+    print(f.cheapest_flight_within_k_stops(
+        5,
+        [[4, 1, 1], [1, 2, 3], [0, 3, 2], [0, 4, 10], [3, 1, 1], [1, 4, 3]],
+        2,
+        1,
+        1,
     ))
