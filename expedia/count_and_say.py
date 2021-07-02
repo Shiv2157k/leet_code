0e371162c0d25806ca4147c0234fe9e74c59274a
Shiv2157k <shiv2157.k@gmail.com>
2021-07-01 23:31:08 -0500

    feat count and say

--- expedia/count_and_say.py
+++ expedia/count_and_say.py
@@ -5,6 +5,32 @@ class Integer:
 
     def count_and_say(self, n: int) -> str:
         """
+        Approach: Iterative
+
+        :param n:
+        :return:
+        """
+        seq = ["1"]
+
+        for digit in range(2, n + 1):
+            next_seq = []
+            digit_count = 1
+            prev_digit = seq[0]
+
+            for d in seq[1:]:
+
+                if d == prev_digit:
+                    digit_count += 1
+                else:
+                    next_seq.extend([str(digit_count), prev_digit])
+                    digit_count = 1
+                    prev_digit = d
+            next_seq.extend([str(digit_count), prev_digit])
+            seq = next_seq[:]
+        return "".join(seq)
+
+    def count_and_say_(self, n: int) -> str:
+        """
         Approach: Recursion
         :param n:
         :return:
@@ -34,3 +60,4 @@ class Integer:
 if __name__ == "__main__":
     integer = Integer()
     print(integer.count_and_say(4))
+    print(integer.count_and_say_(4))
